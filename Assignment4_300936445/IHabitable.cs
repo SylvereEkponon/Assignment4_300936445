0c080679c285052bc6605dd6c3296aeeff264fb9
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-07-13 23:38:10 -0400

    Added IHabitable interface

---
+++ Assignment4_300936445/IHabitable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Sylvere Ekponon
+ * Date: July 13, 2017
+ * Description: This is an Interface that includes a method header Habitable
+ * that must be implemented in any class that subscribes to it.
+ * version: 0.1 - Created IHabitable interface
+ */
+
+namespace Assignment4_300936445
+{ 
+    /// <summary>
+    /// This is the IHabitable interface
+    /// </summary>
+    public interface IHabitable
+    {
+
+        //PUBLIC METHODS
+        bool Habitable();
+    }
+}
