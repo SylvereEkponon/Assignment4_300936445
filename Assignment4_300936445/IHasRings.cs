bd1ccc85a7bf1e1e5ff225286b4dd2f045db5b33
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-07-13 23:11:55 -0400

    Added IHasRings interface

---
+++ Assignment4_300936445/IHasRings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Sylvere Ekponon
+ * Date: July 13, 2017
+ * Description: This is an Interface that includes a method header HasRings
+ * that must be implemented in any class that subscribes to it.
+ * version: 0.1 - Created IHasRings interface
+ */
+
+
+namespace Assignment4_300936445
+{
+    /// <summary>
+    /// This is the IHasRings interface
+    /// </summary>
+    public interface IHasRings
+    {
+
+        //PUBLIC METHODS
+        bool HasRings();
+    }
+}
