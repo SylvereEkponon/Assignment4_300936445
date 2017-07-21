b1b712dccc58e179c39fe003d7163d0cdc137473
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-07-20 21:34:06 -0400

    Fixed bugs in the Planet class

--- Assignment4_300936445/Planet.cs
+++ Assignment4_300936445/Planet.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
  * Description: This is the Abstract Planet class
  * version: 0.1 - Created the Abstract Planet class 
  *              - Overridden the ToString Method
+ *  version: 0.2 - Fixed bugs in the Planet class            
  */            
 
 namespace Assignment4_300936445
@@ -30,13 +31,71 @@ namespace Assignment4_300936445
         private double _rotationPeriod;
 
         //PUBLIC PROPERTIES
-        public double Diameter { get; }
-        public double Mass { get; }
-        public int MoonCount { get; set; }
-        public string Name { get; }
-        public double OrbitalPeriod { get; set; }
-        public int RingCount { get; set; }
-        public double RotationPeriod { get; set; }
+        public double Diameter
+        {
+            get
+            {
+                return this._diameter;
+            }
+        }
+        public double Mass
+        {
+            get
+            {
+                return this._mass;
+            }
+        }
+        
+        public int MoonCount {
+            get
+            {
+                return this._moonCount;
+            }
+            set
+            {
+                this._moonCount = value;
+
+            }
+        }
+        public string Name {
+            get
+            {
+                return this._name;
+            }
+        }
+        public double OrbitalPeriod {
+            get
+            {
+                return this._orbitalPeriod;
+            }
+            set
+            {
+                this._orbitalPeriod = value;
+            }
+        }
+        public int RingCount
+        {
+            get
+            {
+                return this._ringCount;
+            }
+            set
+            {
+                this._ringCount = value;
+            }
+        }
+        public double RotationPeriod
+        {
+            get
+            {
+                return this._rotationPeriod;
+            }
+            set
+            {
+                this._rotationPeriod = value;
+            }
+        }
+        
 
 
         //CONSTRUCTOR---------------------------------------------
--- Assignment4_300936445/Program.cs
+++ Assignment4_300936445/Program.cs
@@ -9,7 +9,8 @@ using System.Threading.Tasks;
  * Name: Sylvere Ekponon
  * Date: July 12, 2017
  * Description: Assignment 4
- * version: 0.1 - Created the Driver class 
+ * version: 0.1 - Created the Driver class
+ * version: 0.2 - Implemented the GiantPlanet Class
  */
 namespace Assignment4_300936445
 {
@@ -17,6 +18,8 @@ namespace Assignment4_300936445
     {
         static void Main(string[] args)
         {
+            GiantPlanet giantPlanet = new GiantPlanet("Jupiter", 142984, 317.8, "ice");
+            Console.WriteLine(giantPlanet.ToString());
         }
     }
 }
