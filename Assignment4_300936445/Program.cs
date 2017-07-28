26e70fb10c4443fea57b7f02ba6371f3eddd4161
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-07-28 12:23:59 -0400

    Tested the HasRings, and HasMoons Methods in the driver class

--- Assignment4_300936445/Program.cs
+++ Assignment4_300936445/Program.cs
@@ -14,6 +14,7 @@ using System.Threading.Tasks;
  * version: 0.3 - Added WaitForAnyKey Method
  * version: 0.4 - Implemented the TerrestrialPlanet Class
  * version: 0.5 - Tested the Habitable Method
+ * version: 0.6 - Tested the HasRings, and HasMoons Methods
  * Git Repo: https://github.com/SylvereEkponon/Assignment4_300936445
  */
 namespace Assignment4_300936445
@@ -25,11 +26,22 @@ namespace Assignment4_300936445
             Console.WriteLine("=====================================================================");
             GiantPlanet giantPlanet = new GiantPlanet("Jupiter", 142984, 317.8, "Gas");
             Console.WriteLine(giantPlanet.ToString());
-            Console.WriteLine("=====================================================================");
+            //Console.WriteLine("=====================================================================");
+
+
+            
+            giantPlanet.MoonCount = 69;
+            giantPlanet.RingCount = 4;
+
+            Console.WriteLine("{0} has {1} Moon(s)", giantPlanet.Name, (giantPlanet.HasMoons()) ? giantPlanet.MoonCount : 0);
+            Console.WriteLine("{0} has {1} Ring(s)", giantPlanet.Name, (giantPlanet.HasRings()) ? giantPlanet.RingCount : 0);
+            Console.WriteLine("\n=====================================================================");
+
             TerrestrialPlanet terrestrialPlanet = new TerrestrialPlanet("Mercury", 4879, 0.055, true);
             Console.WriteLine(terrestrialPlanet.ToString());
-
+            //Console.WriteLine("=====================================================================");
             Console.WriteLine("{0} is {1}an habitable Planet", terrestrialPlanet.Name, (terrestrialPlanet.Habitable()) ? string.Empty : "not ");
+            Console.WriteLine("=====================================================================");
 
             WaitForAnyKey();
         }
