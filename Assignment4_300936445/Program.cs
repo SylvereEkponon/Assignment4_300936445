bfe33269c82aa3fb04a5e52940f03a2bc09ac05f
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-07-28 12:08:49 -0400

    Tested the Habitable method in the driver class

--- Assignment4_300936445/Program.cs
+++ Assignment4_300936445/Program.cs
@@ -13,6 +13,7 @@ using System.Threading.Tasks;
  * version: 0.2 - Implemented the GiantPlanet Class
  * version: 0.3 - Added WaitForAnyKey Method
  * version: 0.4 - Implemented the TerrestrialPlanet Class
+ * version: 0.5 - Tested the Habitable Method
  * Git Repo: https://github.com/SylvereEkponon/Assignment4_300936445
  */
 namespace Assignment4_300936445
@@ -22,11 +23,13 @@ namespace Assignment4_300936445
         static void Main(string[] args)
         {
             Console.WriteLine("=====================================================================");
-            GiantPlanet giantPlanet = new GiantPlanet("Jupiter", 142984, 317.8, "ice");
+            GiantPlanet giantPlanet = new GiantPlanet("Jupiter", 142984, 317.8, "Gas");
             Console.WriteLine(giantPlanet.ToString());
             Console.WriteLine("=====================================================================");
             TerrestrialPlanet terrestrialPlanet = new TerrestrialPlanet("Mercury", 4879, 0.055, true);
             Console.WriteLine(terrestrialPlanet.ToString());
+
+            Console.WriteLine("{0} is {1}an habitable Planet", terrestrialPlanet.Name, (terrestrialPlanet.Habitable()) ? string.Empty : "not ");
 
             WaitForAnyKey();
         }
